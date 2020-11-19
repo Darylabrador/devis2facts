94d916571b9342472b82915375e321fd152e1534
Benjamin974 <benjaminjulienne0@gmail.com>
2020-11-19 11:07:00 +0400

    Ajout des models et des seeders BENZI

---
+++ app/Models/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $table = 'products';
+    protected $fillable = ['name', 'default_price'];
+    public $timestamps = false;
+
+    public function ligneDevis()
+    {
+        return $this->hasMany(User::class, 'product_id');
+    }
+}
