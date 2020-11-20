e39dc4e96c2205904a4977ba0976bad4392cccfe
Gilles974 <boyer.gilles@live.fr>
2020-11-20 14:23:32 +0400

    ajout de l'affichage devis + filtre

--- resources/js/application/views/ListDevis.vue
+++ resources/js/application/views/ListDevis.vue
@@ -1,5 +1,52 @@
 <template>
-    <div>
-        toto
+  <div>
+    <div class="text-center">
+      <h1>Liste des Devis</h1>
     </div>
+
+    <v-card>
+      <v-card-title>
+        <v-btn class="mr-4" @click="$router.push({ path: 'devis' })">
+            Ajouter un Devis +
+        </v-btn>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        >
+        </v-text-field>
+      </v-card-title>
+      <v-data-table :headers="headers" :items="info" :search="search">
+        <template v-slot:item.filename="{ item }">
+          {{ format(item.filename) }}
+        </template>
+        <template v-slot:item.client="{ item }">
+          {{ item.client.name }}
+        </template>
+        <template v-slot:item.is_accepted="{ item }">
+          {{ item.is_accepted }}
+        </template>
+        <template v-slot:item.carbs="{ item }">
+          <v-row class="d-flex flex-row-reverse">
+            <v-btn icon>
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+            <v-btn icon>
+              <v-icon>mdi-email</v-icon>
+            </v-btn>
+            <v-btn icon>
+              <v-icon>mdi-file-download</v-icon>
+            </v-btn>
+            <v-btn icon>
+              <v-icon>mdi-eye-settings</v-icon>
+            </v-btn>
+          </v-row>
+        </template>
+      </v-data-table>
+    </v-card>
+  </div>
 </template>
+
+<script src="./listDevis.js" />
