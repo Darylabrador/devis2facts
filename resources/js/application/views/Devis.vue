c6af28ce91323e0984e61dab537f22aad7689519
Benjamin974 <benjaminjulienne0@gmail.com>
2020-11-30 08:24:42 +0400

    modif devis [en cours] BENZI

--- resources/js/application/views/Devis.vue
+++ resources/js/application/views/Devis.vue
@@ -8,8 +8,8 @@
             <div>Date d'éxpiration : {{ expiration }}</div>
           </v-card>
         </v-col>
-        <v-col md="4"> </v-col>
-        <v-col md="4">
+        <v-col md="2"> </v-col>
+        <v-col md="6">
           <v-card class="pa-5 text-left" outlined>
             <v-simple-table>
               <template v-slot:default>
@@ -24,51 +24,59 @@
                   </tr>
                   <tr>
                     <td>Remise (en %)</td>
-                    <td><v-text-field v-model='remise' type='number' :rules="pourcentRule" @change='emis(remise)' min='0' max='100'></v-text-field></td>
+                    <td>
+                      <v-text-field
+                        v-model="remise"
+                        type="number"
+                        :rules="pourcentRule"
+                        @change="emis(remise)"
+                        min="0"
+                        max="100"
+                      ></v-text-field>
+                    </td>
                   </tr>
                 </tbody>
               </template>
             </v-simple-table>
           </v-card>
+          <facturation :factures='factures'/>
         </v-col>
       </v-row>
-      <v-row class="justify-center mt-10">
-        <v-data-table
-          :headers="headers"
-          :items="lignes"
-          :items-per-page="10"
-          hide-default-footer
-          class="elevation-1"
-        >
-          <template v-slot:header="{ props: { headers } }">
-            <thead>
-              <tr>
-                <th :colspan="headers.length">
-                  <v-row>
-                    <v-col md="4"
-                      ><addLigne @addLigne="add($event)"
-                    /></v-col>
-                    <v-col md="4">
-                      <!-- <autocomplete /> -->
-                      </v-col>
-                    <v-col md="4"><tva /></v-col>
-                  </v-row>
-                </th>
-              </tr>
-            </thead>
-          </template>
-          <template v-slot:item.product="{ item }"> {{ item.name }}</template>
-          <template v-slot:item.quantity="{ item }">{{ item.quantity }}</template>
-          <template v-slot:item.description="{ item }">{{item.description}}</template>
-          <template v-slot:item.price="{ item }">{{ item.price }}</template>
-          <template v-slot:item.total="{ item }"
-            >{{ item.quantity * item.price }}
-          </template>
-          <template v-slot:item.check="{ item }">
-            <v-checkbox class="mb-2" v-model="checkbox" />
-          </template>
-        </v-data-table>
-      </v-row>
+      <v-data-table
+        :headers="headers"
+        :items="lignes"
+        :items-per-page="10"
+        hide-default-footer
+        class="elevation-1"
+      >
+        <template v-slot:header="{ props: { headers } }">
+          <thead>
+            <tr>
+              <th :colspan="headers.length">
+                <v-row>
+                  <v-col md="4"><addLigne @addLigne="add($event)" /></v-col>
+                  <v-col md="4">
+                    <!-- <autocomplete /> -->
+                  </v-col>
+                  <v-col md="4"><tva /></v-col>
+                </v-row>
+              </th>
+            </tr>
+          </thead>
+        </template>
+        <template v-slot:item.product="{ item }"> {{ item.name }}</template>
+        <template v-slot:item.quantity="{ item }">{{ item.quantity }}</template>
+        <template v-slot:item.description="{ item }">{{
+          item.description
+        }}</template>
+        <template v-slot:item.price="{ item }">{{ item.price }}</template>
+        <template v-slot:item.total="{ item }"
+          >{{ item.quantity * item.price }}
+        </template>
+        <template v-slot:item.check="{ item }">
+          <v-checkbox class="mb-2" v-model="checkbox" />
+        </template>
+      </v-data-table>
     </template>
   </v-container>
 </template>
--- resources/js/application/views/devis.js
+++ resources/js/application/views/devis.js
@@ -2,11 +2,13 @@ import Axios from "axios"
 import Tva from '../components/devis/Tva.vue'
 import Autocomplete from '../components/devis/Autocomplete.vue'
 import AddLigne from '../components/devis/AddLigne.vue'
+import Facturation from '../components/devis/lignedevis/Facturation.vue'
 export default {
     components: {
         Tva,
         Autocomplete,
-        AddLigne
+        AddLigne,
+        Facturation
     },
 
 
@@ -33,40 +35,40 @@ export default {
             ],
             lignes: [],
             tht: 0,
-            ttc:0,
+            ttc: 0,
 
-            valuetht:0,
-            valuettc:0,
+            valuetht: 0,
+            valuettc: 0,
 
-            remise:0,
+            remise: 0,
             creation: '',
             expiration: '',
+            factures: []
 
         }
     },
     created() {
         this.getLigne()
         this.getDevis()
-        
-        
+
+
     },
     methods: {
         getLigne() {
             Axios.get('/api/devis/find/ligne/' + this.$route.params.id).then(({ data }) => {
 
                 data.data.forEach(ligne => {
+                    this.getFacture(ligne);
                     this.lignes.push(ligne)
                     this.tht += ligne.price * ligne.quantity
-                    this.ttc += ( ligne.price + ligne.price * ligne.devis.tva / 100 ) * ligne.quantity
-                    
+                    this.ttc += (ligne.price + ligne.price) * ligne.quantity
+                   
                 })
                 this.valuetht = this.tht
                 this.valuettc = this.ttc
 
             })
 
-           
-                
         },
 
         getDevis() {
@@ -79,16 +81,21 @@ export default {
         add(ligne) {
             this.lignes.push(ligne)
             this.tht += ligne.price * ligne.quantity
-            this.ttc += ( ligne.price + ligne.price * ligne.devis.tva / 100 ) * ligne.quantity
+            this.ttc += (ligne.price + ligne.price) * ligne.quantity
 
             this.valuetht = this.tht
             this.valuettc = this.ttc
         },
 
-        emis(value) {
+        emis(remise) {
+            this.valuettc = this.ttc - this.ttc * remise / 100
+            this.valuetht = this.tht - this.tht * remise / 100
+        },
 
-            this.valuetht = this.ttc*value/100
-            this.valuettc = this.tht*value/100
+        getFacture(ligne) {
+            if (ligne.facture.is_paid == 1) {
+                this.factures.push(ligne.facture.filename)
+            } 
         }
 
     }
