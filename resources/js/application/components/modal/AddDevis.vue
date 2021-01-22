dca7fcbf6424167d53bfc8ee60fb34bc85e5eb70
Darylabrador <abrador.daryl@gmail.com>
2021-01-22 22:48:06 +0400

    Correctif date expiration devis

--- resources/js/application/components/modal/AddDevis.vue
+++ resources/js/application/components/modal/AddDevis.vue
@@ -38,34 +38,11 @@
 							</v-row>
 						</v-col>
 						<v-col cols="12" sm="6" md="4">
-							<v-dialog
-								ref="dialog"
-								v-model="modal"
-								:return-value.sync="dateM"
-								persistent
-								width="290px"
-							>
-								<template v-slot:activator="{ on, attrs }">
-									<v-text-field
-										v-model="dateM"
-										color="#f90"
-										label="date expiration"
-										prepend-icon="mdi-calendar"
-										readonly
-										v-bind="attrs"
-										v-on="on"
-									></v-text-field>
-								</template>
-								<v-date-picker v-model="dateM" scrollable>
-									<v-spacer></v-spacer>
-									<v-btn text color="primary" @click="modal = false">
-										Cancel
-									</v-btn>
-									<v-btn text color="primary" @click="$refs.dialog.save(dateM)">
-										OK
-									</v-btn>
-								</v-date-picker>
-							</v-dialog>
+							<v-text-field
+								v-model="dateExp"
+								label="Date d'expiration"
+								readonly
+							></v-text-field>
 						</v-col>
 
 						<v-col cols="12" sm="6" md="4">
--- resources/js/application/components/modal/js/AddDevis.js
+++ resources/js/application/components/modal/js/AddDevis.js
@@ -13,13 +13,12 @@ export default {
             date: '',
             isAcompte: false,
             dialog: false,
-
-            dateM: '',
             menu: false,
             modal: false,
             menu2: false,
         }
     },
+
     created() {
         this.getClients()
         this.initialize()
@@ -27,27 +26,35 @@ export default {
     },
 
     methods: {
-        addDevis() {
+        formatDate(date) {
+            if (!date) return null
+            const [year, month, day] = date.split('-')
+            let yearFormat = Number(year);
+            yearFormat += 1;
+            return `${day}/${month}/${yearFormat}`
+        },
+        parseDate(date) {
+            if (!date) return null
+            const [day, month, year] = date.split('/')
+            return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
+        },
 
-            Axios.post('/api/devis/add', { is_acompte: this.isAcompte, client_id: this.selectedClient, filename: this.devisFilename, remise: 0, tva: this.tva, date_expiration: this.dateM }).then(({data}) => {
+        addDevis() {
+            Axios.post('/api/devis/add', { is_acompte: this.isAcompte, client_id: this.selectedClient, filename: this.devisFilename, remise: 0, tva: this.tva, date_expiration: this.parseDate(this.dateExp) }).then(({data}) => {
                 if (this.isAcompte) {
                     this.$router.push('devis/acompte/' + data.data.id);
 
                 } else {
                     this.$router.push('devis/' + data.data.id);
-
                 }
-
             })
-
-
         },
-
 
         initialize() {
-            this.date = new Date()
-            this.dateM = (this.date.getFullYear() + 1) + '-' + this.date.getMonth() + '-' + this.date.getDay()
+            this.date = new Date();
+            this.dateExp = this.formatDate(new Date().toISOString().substr(0, 10))
         },
+
         getClients() {
             Axios.get('api/clients/getAll').then(({ data }) => {
 
@@ -57,15 +64,12 @@ export default {
 
             })
         },
+
         getLastIdDevis() {
             Axios.get('api/devis/lastIdDevis').then(({ data }) => {
-                //this.devisLastId = data.data.id
                 this.devisFilename = 'DE-' + this.date.getFullYear() + '-' + this.date.getMonth() + '-' + (data.data.id + 1)
             })
 
         },
-
-
     },
-
 }
